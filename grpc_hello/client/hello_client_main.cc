49baf2ddd114e7988d5619bcd8bc384d6a9c1902
starlet <starlet@localhost>
2022-02-07 00:59:54 +0900

    Added grpc_hello client and its test.

---
+++ grpc_hello/client/hello_client_main.cc
@@ -0,0 +1,40 @@
+#include "grpc_hello/client/hello_client.h"
+
+#include <cstdlib>
+#include <string>
+
+#include "gflags/gflags.h"
+#include "glog/logging.h"
+#include "grpcpp/grpcpp.h"
+#include "grpcpp/health_check_service_interface.h"
+
+DEFINE_string(host, "0.0.0.0:8080", "Host ip and port.");
+DEFINE_string(id, "darling", "Id.");
+
+namespace grpc_hello {
+namespace client {
+
+using ::grpc::Server;
+using ::grpc::ServerBuilder;
+using ::grpc::ServerContext;
+using ::grpc::Status;
+using ::grpc_hello::client::HelloServiceClient;
+
+void RunClient() {
+  HelloServiceClient client(::grpc::CreateChannel(FLAGS_host, ::grpc::InsecureChannelCredentials()));
+  std::string response_string = client.Hello(FLAGS_id);
+
+  LOG(INFO) << "Message received: " << response_string;
+}
+
+}  // namespace client
+}  // namespace grpc_hello
+
+int main(int argc, char **argv) {
+  ::google::InitGoogleLogging(argv[0]);
+  ::google::ParseCommandLineFlags(&argc, &argv, true);
+
+  ::grpc_hello::client::RunClient();
+
+  return EXIT_SUCCESS;
+}
