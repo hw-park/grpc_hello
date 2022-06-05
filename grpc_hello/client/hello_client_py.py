83524ea5a87c5b2dec3430d99269072e2e0a77a7
starlet <starlet@localhost>
2022-06-06 02:41:36 +0900

    Added python version of hello_client.

---
+++ grpc_hello/client/hello_client_py.py
@@ -0,0 +1,17 @@
+from absl import logging
+from grpc_hello.protos import grpc_hello_pb2_grpc
+from grpc_hello.protos import grpc_hello_pb2
+import grpc
+
+
+class HelloServiceClient:
+    def __init__(self, channel) -> None:
+        self._stub = grpc_hello_pb2_grpc.HelloServiceStub(channel)
+
+    def hello(self, client_id: str) -> str:
+        request = grpc_hello_pb2.HelloRequest(id=client_id)
+        response = self._stub.Hello(request)
+        if response is None:
+            logging.warning('HelloService RPC failed.')
+            return 'RPC failed'
+        return response.response
