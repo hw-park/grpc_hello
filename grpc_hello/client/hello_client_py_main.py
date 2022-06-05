9fe7d29cf7fa7fe0a20400247c57df1c35712199
starlet <starlet@localhost>
2022-06-06 02:41:36 +0900

    Added python version of hello_client.

---
+++ grpc_hello/client/hello_client_py_main.py
@@ -0,0 +1,22 @@
+from absl import app
+from absl import flags
+from absl import logging
+import grpc
+import hello_client_py
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('host', '0.0.0.0:8080', 'Host ip and port.')
+flags.DEFINE_string('id', 'darling', 'Id.')
+
+
+def main(argv):
+    client = hello_client_py.HelloServiceClient(
+        grpc.insecure_channel(FLAGS.host))
+    response_string = client.hello(FLAGS.id);
+
+    logging.info('Message received: ' + FLAGS.id)
+
+
+if __name__ == '__main__':
+    app.run(main)
